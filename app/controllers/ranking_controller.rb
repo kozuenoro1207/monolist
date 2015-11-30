3abd47691df95676754875fda355c6b69a558497
kozue_noro <kozuenoro1207@gmail.com>
2015-11-30 11:43:48 +0000

    [modify] ranking_item_method

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,11 +1,11 @@
 class RankingController < ApplicationController
   def want
-    @rank_wants_id = Item.joins(:wants).group(:item_id).count(:user_id).sort_by{|k,v| v}.take(10).to_h.keys
-    @rank_wants_item = Item.find(@rank_wants_id)
+    @rank_wants_id = Item.joins(:wants).group(:item_id).order('count_item_id desc').count(:item_id).keys.take(10)
+    @rank_wants_item = Item.find(@rank_wants_id).index_by(&:id).slice(*@rank_wants_id).values
 
   end
   def have
-    @rank_haves_id = Item.joins(:haves).group(:item_id).count(:user_id).sort_by{|k,v| v}.take(10).to_h.keys
-    @rank_haves_item = Item.find(@rank_haves_id)
+    @rank_haves_id = Item.joins(:haves).group(:item_id).order('count_item_id desc').count(:item_id).keys.take(10)
+    @rank_haves_item = Item.find(@rank_haves_id).index_by(&:id).slice(*@rank_haves_id).values
   end
 end
